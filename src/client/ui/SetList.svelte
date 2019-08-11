cc44b57dd6c8174a1ad2a8ad7b07360f622a7fb3
Wangchou Lu <kindalu@gmail.com>
2019-08-11 12:09:14 +0800

    tag clickable

--- src/client/ui/SetList.svelte
+++ src/client/ui/SetList.svelte
@@ -1,15 +1,47 @@
 <script>
     import { sentenceSets, idToRow } from '../model/demoSets.js'
 
-    console.log(idToRow)
-    var selectedTags = ["戀愛"]
-    var filteredSets = selectedTags.length == 0 ? sentenceSets :
-                                                  sentenceSets.filter(set => {selectedTags.includes(set.tag)})
+    //console.log(idToRow)
+    var tagCounts = {}
+    sentenceSets.forEach(set => {
+        tagCounts[set.tag] = tagCounts[set.tag] ? (tagCounts[set.tag] + 1) : 1
+    })
+    var tags = Object.keys(tagCounts)
+    tags.sort((tag1, tag2) => {
+        return tagCounts[tag2] - tagCounts[tag1]
+    })
+
+    var isSelectedTag = {}
+    $: isAllTag = !Object.values(isSelectedTag).includes(true)
+    $: filteredSets = isAllTag ? sentenceSets :
+                                 sentenceSets.filter(set => isSelectedTag[set.tag])
+
+    function toggleTag(tag) {
+        if (isSelectedTag[tag]) {
+            isSelectedTag[tag] = false
+        } else {
+            isSelectedTag[tag] = true
+        }
+        console.log(isSelectedTag)
+    }
 </script>
 
+<div class="tagContainer">
+    {#if isAllTag}
+        <div class="tag selected">全部({sentenceSets.length})</div>
+    {:else}
+        <div class="tag" on:click={() => isSelectedTag={}}>全部({sentenceSets.length})</div>
+    {/if}
+    {#each tags as tag}
+        <div class={`tag ${isSelectedTag[tag] ? "selected" : ""}`}
+             on:click={()=> {toggleTag(tag)}}>
+            {`${tag}(${tagCounts[tag]})`}
+        </div>
+    {/each}
+</div>
 
 <div class="cardContainer">
-     {#each sentenceSets as set}
+     {#each filteredSets as set}
         <div class="card">
             <div class="difficultyLabel">{set.difficultyLabel}</div>
             <div class="title">
@@ -25,6 +57,22 @@
 </div>
 
 <style>
+    .tagContainer {
+        padding: 0px 10px 5px;
+    }
+    .tag {
+        display: inline-block;
+        font-size: 12px;
+        padding: 1px 2px;
+        margin: 0px 2px;
+    }
+    .tag:hover {
+        background: #f6faf3;
+    }
+    .tag.selected, .tag.selected:hover {
+        color: white;
+        background: #60a030;
+    }
     .cardContainer {
         width: 100%;
         height: 100%;
