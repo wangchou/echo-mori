c1cd554e74f6e6152b5bbc7c9c7abfc87ea9f3f3
WangChou Lu <wangchou@users.noreply.github.com>
2019-08-31 15:13:28 +0800

    Revert "Record"

--- src/server/api/index.mjs
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { record } from './record'
-import { user } from './user'
-
-const api = express.Router()
-api.use('/record', record)
-api.use('/user', user)
-
-export { api }
